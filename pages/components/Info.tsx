cefb500f8fb07ed51814c5a40d27009c15cf8277
CenturyNine <lowlifeix@gmail.com>
2023-09-12 18:52:03 +0700

    image slider

--- pages/components/Info.tsx
+++ pages/components/Info.tsx
@@ -1,5 +1,31 @@
 import React from "react";
 import TypewriterComponent from "typewriter-effect";
+import ImageGallery from "react-image-gallery";
+
+const images = [
+ 
+  {
+    original: "https://i.ibb.co/mJXZJ3t/Screenshot-1694452768.png",
+    thumbnail: "https://i.ibb.co/mJXZJ3t/Screenshot-1694452768.png",
+  },
+  {
+    original: "https://i.ibb.co/9hdjw5P/Screenshot-1694452849.png",
+    thumbnail: "https://i.ibb.co/9hdjw5P/Screenshot-1694452849.png",
+  },
+  {
+    original: "https://i.ibb.co/5RqbSbq/Screenshot-1694452866.png",
+    thumbnail: "https://i.ibb.co/5RqbSbq/Screenshot-1694452866.png",
+  },
+  {
+    original: "https://i.ibb.co/dgJpnwT/Screenshot-1694452881.png",
+    thumbnail: "https://i.ibb.co/dgJpnwT/Screenshot-1694452881.png",
+  },
+  {
+    original: "https://i.ibb.co/MMHDXcp/Screenshot-1694452883.png",
+    thumbnail: "https://i.ibb.co/MMHDXcp/Screenshot-1694452883.png",
+  },
+];
+
 
 function Info() {
   return (
@@ -16,10 +42,12 @@ function Info() {
                 loop: true,
               }}
             />
+               
           </span>
           <div className=" flex flex-row gap-2 justify-center text-3xl md:text-4xl font-medium bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-blue-400">
-            <span className="">Flood Warning System Around the Drain Area</span>
+            <span className="mb-32">Flood Warning System Around the Drain Area</span>
           </div>
+          <ImageGallery items={images} />;
           <p className="text-slate-700 text-1xl md:text-2xl mr-5 text-center"></p>
         </div>
         <div className="xl:mr-56 relative ">
